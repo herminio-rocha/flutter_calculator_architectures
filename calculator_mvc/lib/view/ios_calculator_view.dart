0e28f95bf82d2748747522ff412d0a72069429df
Herminio Rocha <herminiorocha@MacBook-Air-de-Herminio.local>
2025-01-28 19:19:29 -0300

    feat: complete ios numeric keypad layout

--- calculator_mvc/lib/view/ios_calculator_view.dart
+++ calculator_mvc/lib/view/ios_calculator_view.dart
@@ -1,4 +1,7 @@
+import 'package:calculator_mvc/utils/button_config.dart';
+import 'package:calculator_mvc/view/widgets/calculator_button.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class IosCalculatorView extends StatefulWidget {
   const IosCalculatorView({super.key});
@@ -10,9 +13,58 @@ class IosCalculatorView extends StatefulWidget {
 class _IosCalculatorViewState extends State<IosCalculatorView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("IOS"),
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.grey[850],
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.amber,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: GridView.count(
+              padding: EdgeInsets.fromLTRB(
+                MediaQuery.of(context).size.width * 0.03,
+                MediaQuery.of(context).size.height * 0.03,
+                MediaQuery.of(context).size.width * 0.03,
+                MediaQuery.of(context).size.height * 0.03,
+              ),
+              crossAxisSpacing: MediaQuery.of(context).size.width * 0.025,
+              mainAxisSpacing: MediaQuery.of(context).size.height * 0.025,
+              crossAxisCount: 4,
+              children: [
+                ...[
+                  ButtonConfig.clear,
+                  ButtonConfig.backspace,
+                  ButtonConfig.percent,
+                  ButtonConfig.divide,
+                  ButtonConfig.seven,
+                  ButtonConfig.eight,
+                  ButtonConfig.nine,
+                  ButtonConfig.multiply,
+                  ButtonConfig.four,
+                  ButtonConfig.five,
+                  ButtonConfig.six,
+                  ButtonConfig.subtract,
+                  ButtonConfig.one,
+                  ButtonConfig.two,
+                  ButtonConfig.three,
+                  ButtonConfig.add,
+                  ButtonConfig.doubleZero,
+                  ButtonConfig.zero,
+                  ButtonConfig.dot,
+                  ButtonConfig.equal
+                ].map(
+                  (buttonConfig) =>
+                      CalculatorButton(buttonConfig: buttonConfig),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
