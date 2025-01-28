7852c8fec961b78b629ba80193b102f1cf3b5d5b
Herminio Rocha <herminiorocha@MacBook-Air-de-Herminio.local>
2025-01-28 17:05:17 -0300

    chore: change flutter version

--- calculator_mvc/android/app/src/main/kotlin/br/com/mahatechnology/calculator_mvc/MainActivity.kt
+++ calculator_mvc/android/app/src/main/kotlin/br/com/mahatechnology/calculator_mvc/MainActivity.kt
@@ -2,5 +2,4 @@ package br.com.mahatechnology.calculator_mvc
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
