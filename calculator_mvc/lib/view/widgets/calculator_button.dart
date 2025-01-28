04ec9621d509a53e657f9b03a628468ebe1d45c9
Herminio Rocha <herminiorocha@MacBook-Air-de-Herminio.local>
2025-01-28 19:20:01 -0300

    refactor: Adjust CalculatorButton to integrate with ButtonConfig

--- calculator_mvc/lib/view/widgets/calculator_button.dart
+++ calculator_mvc/lib/view/widgets/calculator_button.dart
@@ -1,46 +1,53 @@
+import 'dart:io';
+
+import 'package:calculator_mvc/utils/button_config.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
-  final String label;
+  final ButtonConfig buttonConfig;
 
-  const CalculatorButton({super.key, required this.label});
+  const CalculatorButton({super.key, required this.buttonConfig});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(0 //MediaQuery.of(context).size.width * 0.015,
-          ),
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.grey[850],
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24),
-            side: BorderSide(
-              color: Colors.grey[900]!.withOpacity(0.5),
-              width: 2,
-            ),
-          ),
-          elevation: 16,
-          shadowColor: Colors.black,
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Text(
-                  label,
-                  style: const TextStyle(
-                    fontSize: 50,
-                    fontFamily: "Courier New",
-                  ),
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor:
+            buttonConfig.backgroundColor, //colorButton ?? Colors.grey[850]!,
+        foregroundColor: buttonConfig.textColor,
+        shape: Platform.isAndroid
+            ? RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+                side: BorderSide(
+                  color: buttonConfig.borderColor,
+                  width: 2,
+                ),
+              )
+            : CircleBorder(
+                side: BorderSide(
+                  color: buttonConfig.borderColor,
+                  width: 2,
                 ),
               ),
-            )
-          ],
-        ),
+        elevation: Platform.isAndroid ? 16 : 0,
+        shadowColor: Colors.black,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                buttonConfig.label,
+                style: const TextStyle(
+                  fontSize: 50,
+                  fontFamily: "Courier New",
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
