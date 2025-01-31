a45d4ddd08d7c2e253d28f61b3e7e0b969c3e315
Herminio Rocha <herminiorocha@MacBook-Air-de-Herminio.local>
2025-01-31 13:20:01 -0300

    feat: add equation and result variables with retrieval functions

--- calculator_mvc/lib/model/calculator_model.dart
+++ calculator_mvc/lib/model/calculator_model.dart
@@ -1,8 +1,18 @@
+import 'package:calculator_mvc/model/calculation.dart';
 import 'package:flutter/foundation.dart';
 
 class CalculatorModel extends ChangeNotifier {
-  String display = "";
-  double result = 0.0;
+  final Calculation _calculation = Calculation(
+    equationString: "",
+    equationValue: 0.0,
+    resultString: "0",
+    resultValue: 0.0,
+  );
+
+  String get equationString => _calculation.equationString;
+  double get equationValue => _calculation.equationValue;
+  String get resultString => _calculation.resultString;
+  double get resultValue => _calculation.resultValue;
 
   void clear() {}
 
