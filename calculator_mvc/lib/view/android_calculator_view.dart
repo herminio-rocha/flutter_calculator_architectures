57f95206b0c737348c171104c6af3c5718404129
Herminio Rocha <herminiorocha@MacBook-Air-de-Herminio.local>
2025-01-31 13:21:41 -0300

    feat: android display equation and result on screen

--- calculator_mvc/lib/view/android_calculator_view.dart
+++ calculator_mvc/lib/view/android_calculator_view.dart
@@ -1,8 +1,10 @@
 import 'package:calculator_mvc/controller/calculator_controller.dart';
 import 'package:calculator_mvc/model/button_config_repository.dart';
 import 'package:calculator_mvc/model/calculator_model.dart';
-import 'package:calculator_mvc/view/widgets/calculator_button.dart';
+import 'package:calculator_mvc/view/widgets/android_text_style.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/android_calculator_button.dart';
 
 class AndroidCalculatorView extends StatefulWidget {
   const AndroidCalculatorView({super.key});
@@ -35,7 +37,47 @@ class _AndroidCalculatorViewState extends State<AndroidCalculatorView> {
           Expanded(
             flex: 1,
             child: Container(
-              color: Colors.amber,
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: SafeArea(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        reverse: true,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Directionality(
+                              textDirection: TextDirection.ltr,
+                              child: Text(
+                                _calculatorController.equationString,
+                                style: androidTextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.13,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    reverse: true,
+                    child: Text(
+                      _calculatorController.resultString,
+                      style: androidTextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.07,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -50,9 +92,10 @@ class _AndroidCalculatorViewState extends State<AndroidCalculatorView> {
               crossAxisSpacing: MediaQuery.of(context).size.width * 0.025,
               mainAxisSpacing: MediaQuery.of(context).size.height * 0.025,
               crossAxisCount: 4,
+              clipBehavior: Clip.hardEdge,
               children: [
                 ...ButtonConfigRepository.buttons.map(
-                  (buttonConfig) => CalculatorButton(
+                  (buttonConfig) => AndroidCalculatorButton(
                     buttonConfig: buttonConfig,
                     calculatorController: _calculatorController,
                   ),
