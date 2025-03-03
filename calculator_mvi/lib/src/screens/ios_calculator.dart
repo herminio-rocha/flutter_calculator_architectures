423e84539ab4da4461447364a0d40d01549eebd9
Herminio Rocha <herminiorocha@192.168.1.78>
2025-03-03 17:13:04 -0300

    refactor: extract widgets to build methods

--- calculator_mvi/lib/src/screens/ios_calculator.dart
+++ calculator_mvi/lib/src/screens/ios_calculator.dart
@@ -19,6 +19,9 @@ class IosCalculatorView extends StatefulWidget {
 class _IosCalculatorViewState extends State<IosCalculatorView> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final screenHeight = MediaQuery.sizeOf(context).height;
+
     return BlocProvider<CalculatorBloc>(
       lazy: false,
       create: (_) => CalculatorBloc(
@@ -35,61 +38,10 @@ class _IosCalculatorViewState extends State<IosCalculatorView> {
               buildWhen: (previus, current) => current is CalculatorLabels,
               builder: (context, state) {
                 if (state is CalculatorLabels) {
-                  return Expanded(
-                    flex: 1,
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            child: SafeArea(
-                              child: SingleChildScrollView(
-                                scrollDirection: Axis.vertical,
-                                reverse: true,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    Directionality(
-                                      textDirection: TextDirection.ltr,
-                                      child: Text(
-                                        state.equation,
-                                        textAlign: TextAlign.end,
-                                        style: iosTextStyle(
-                                          fontSize: state.showResult
-                                              ? MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.07
-                                              : MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.13,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            reverse: true,
-                            child: Text(
-                              state.result,
-                              style: iosTextStyle(
-                                fontSize: state.showResult
-                                    ? MediaQuery.of(context).size.width * 0.13
-                                    : MediaQuery.of(context).size.width * 0.07,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  return _buildDisplay(
+                    screenWidth,
+                    BlocProvider.of<CalculatorBloc>(context),
+                    state,
                   );
                 }
 
@@ -101,31 +53,11 @@ class _IosCalculatorViewState extends State<IosCalculatorView> {
                   current is CalculatorButtonsLoaded,
               builder: (context, state) {
                 if (state is CalculatorButtonsLoaded) {
-                  return Expanded(
-                    flex: 2,
-                    child: GridView.count(
-                      padding: EdgeInsets.fromLTRB(
-                        MediaQuery.of(context).size.width * 0.03,
-                        MediaQuery.of(context).size.height * 0.03,
-                        MediaQuery.of(context).size.width * 0.03,
-                        MediaQuery.of(context).size.height * 0.03,
-                      ),
-                      crossAxisSpacing:
-                          MediaQuery.of(context).size.width * 0.025,
-                      mainAxisSpacing:
-                          MediaQuery.of(context).size.height * 0.025,
-                      crossAxisCount: 4,
-                      clipBehavior: Clip.hardEdge,
-                      children: state.buttons
-                          .map(
-                            (buttonConfig) => IosCalculatorButton(
-                              buttonConfig: buttonConfig,
-                              calculatorBloc:
-                                  BlocProvider.of<CalculatorBloc>(context),
-                            ),
-                          )
-                          .toList(),
-                    ),
+                  return _buildKeypad(
+                    screenWidth,
+                    screenHeight,
+                    BlocProvider.of<CalculatorBloc>(context),
+                    state,
                   );
                 }
 
@@ -134,6 +66,94 @@ class _IosCalculatorViewState extends State<IosCalculatorView> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildDisplay(
+    double screenWidth,
+    CalculatorBloc calculatorBloc,
+    CalculatorLabels state,
+  ) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: SafeArea(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  reverse: true,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: Text(
+                          state.equation,
+                          textAlign: TextAlign.end,
+                          style: iosTextStyle(
+                            fontSize: state.showResult
+                                ? screenWidth * 0.07
+                                : screenWidth * 0.13,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              reverse: true,
+              child: Text(
+                state.result,
+                style: iosTextStyle(
+                  fontSize: state.showResult
+                      ? screenWidth * 0.13
+                      : screenWidth * 0.07,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildKeypad(
+    double screenWidth,
+    double screenHeight,
+    CalculatorBloc calculatorBloc,
+    CalculatorButtonsLoaded state,
+  ) {
+    return Expanded(
+      flex: 2,
+      child: GridView.count(
+        padding: EdgeInsets.fromLTRB(
+          screenWidth * 0.03,
+          screenHeight * 0.03,
+          screenWidth * 0.03,
+          screenHeight * 0.03,
+        ),
+        crossAxisSpacing: screenWidth * 0.025,
+        mainAxisSpacing: screenHeight * 0.025,
+        crossAxisCount: 4,
+        clipBehavior: Clip.hardEdge,
+        children: state.buttons
+            .map(
+              (buttonConfig) => IosCalculatorButton(
+                buttonConfig: buttonConfig,
+                calculatorBloc: calculatorBloc,
+              ),
+            )
+            .toList(),
       ),
     );
   }
